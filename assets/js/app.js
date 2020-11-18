a35c9537da98192ee22c7ed3ccf373d48623106e
Catty <kata.chavez@gmail.com>
2020-11-18 14:07:19 -0300

    Validaciones input

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,107 @@
 const peopleForm = (function () {
+
   const state = {}
   state.people = []
-  
-  const inputBtn  =  document.querySelector('button[type="submit"]')
-  const nameInput = document.querySelector('input[type="text"]')
+  state.errors = []
   const peopleList = document.querySelector('div.results')
-  
+  const nameInput = document.querySelector('input[type="text"]')
+  const inputBtn = document.querySelector('button[type="submit"]')
+
   inputBtn.addEventListener('click', addPerson)
   peopleList.addEventListener('click', removePerson)
-  
-  function removePerson(e){
-    if(!e.target.classList.contains('label-warning')){ return }
+
+  function removePerson(e) {
+    if (!e.target.classList.contains('label-warning')) { return }
 
     const btn = e.target.parentNode
     removeFromStateAndHtml(btn)
   }
-  
-  function apiRemovePerson(str){
+
+  function apiRemovePerson(str) {
     const btn = getButtonByString(str)
     removeFromStateAndHtml(btn)
-  
   }
 
-  function removeFromStateAndHtml(btn){
-    if(!btn){ return }
+  function removeFromStateAndHtml(btn) {
+    if (!btn) { return }
 
     const personName = btn.innerText.split(" x")[0]
     const nameIndex = state.people.indexOf(personName)
-    
-    state.people.splice(nameIndex,1)
-    peopleList.removeChild(btn);
 
+    state.people.splice(nameIndex, 1)
+    peopleList.removeChild(btn);
   }
-   
-  function getButtonByString(str){
-    if(state.people.indexOf(str) > -1){
+
+  function getButtonByString(str) {
+    if (state.people.indexOf(str) > -1) {
       for (const button of document.querySelectorAll("button.btn-success")) {
-            console.log(button.textContent);
-          if (button.textContent.split(" x")[0] == str) {
+        console.log(button.textContent);
+        if (button.textContent.split(" x")[0] == str) {
           return button
         }
       }
     }
   }
-  
-  function addPerson(e){
+
+  function addPerson(e) {
     let personName = e
-    if(typeof(e)=='object'){ 
-      e.preventDefault() 
+
+    if (typeof (e) == 'object') {
+      e.preventDefault()
       personName = nameInput.value
-      };
-    state.people.push(personName)
-    render()
+    };
+
+    if (validate(personName)) {
+      state.people.push(personName)
+      render()
+    } else {
+      alert(state.errors.join(' '))
+    }
+    nameInput.value = ''
   }
-  function render(){
+
+  function validate(personName) {
+    state.errors = []
+    const v1 = validatesOnlyChars(personName)
+    const v2 = validatesUniqueness(personName)
+    const v3 = validatesNotNull(personName)
+
+    return (v1 && v2 && v3)
+  }
+
+  function validatesUniqueness(str) {
+    if (!state.people.some(person => person == str)) {
+      return true
+    } else {
+      state.errors.push('Nombre ingresado ya existe.')
+      return false
+    }
+  }
+
+  function validatesNotNull(str) {
+    if (str == '' || str == null) {
+      state.errors.push('El campo está vacio.')
+      return false
+    }
+    return true
+  }
+
+  function validatesOnlyChars(str) {
+    let regex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g;
+
+    if (regex.test(str)) {
+      return true
+    } else {
+      state.errors.push('El nombre contiene caractéres inválidos.')
+      return false
+    }
+  }
+
+  function render() {
     const lastPerson = state.people[state.people.length - 1]
     const personBtn = document.createElement('button')
     const closeSpan = document.createElement('span')
-    
+
     closeSpan.className = 'label label-warning'
     closeSpan.innerText = ' x'
     personBtn.className = 'btn btn-success mx-2 my-2'
@@ -65,6 +109,7 @@ const peopleForm = (function () {
     personBtn.appendChild(closeSpan)
     peopleList.appendChild(personBtn)
   }
+
+  return { addPerson, removePerson: apiRemovePerson }
   
-  return { addPerson, removePerson: apiRemovePerson}
- })()
+})()
